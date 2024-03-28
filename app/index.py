878ae4bd57c6226d06961bbcf2f4ae3887f63cae
larbing <larbing@163.com>
2024-03-28 13:37:42 +0800

    up

---
+++ app/index.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+            
+__solt__ = [app]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
