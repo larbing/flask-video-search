9ec64417bfa749350a627cb144c466bcf2532e1b
larbing <larbing@163.com>
2024-05-22 16:34:37 +0800

    up

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+import fastwsgi
 from app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0',port=80)
+    fastwsgi.run(wsgi_app=app,host='0.0.0.0',port=80,workers=4)
