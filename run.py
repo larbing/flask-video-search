14564d9d194beab5cb3eea67484e3a2ef30c5bc0
larbing <larbing@163.com>
2024-06-05 09:18:22 +0800

    up

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-import fastwsgi
+import tornado
+import asyncio
 from app import create_app
 
-if __name__ == '__main__':
+async def main():
     app = create_app()
-    fastwsgi.run(wsgi_app=app,host='0.0.0.0',port=80,workers=4)
+    container = tornado.wsgi.WSGIContainer(app)
+    http_server = tornado.httpserver.HTTPServer(container)
+    http_server.listen(80)
+    await asyncio.Event().wait()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+    #fastwsgi.run(wsgi_app=app,host='0.0.0.0',port=80,workers=4)
