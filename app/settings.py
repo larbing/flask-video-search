3ba4774f39044916889f89744b65e725ba72c52e
larbing <larbing@163.com>
2024-09-04 15:29:01 +0800

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -20,6 +20,7 @@ channel_settings = [
     {"t_id":1100,"t_name":"日韩综艺","t_pid":"3","t_title":""},
     {"t_id":1200,"t_name":"港台综艺","t_pid":"3","t_title":""},
     {"t_id":1300,"t_name":"欧美综艺","t_pid":"3","t_title":""},
+    {"t_id":1400,"t_name":"日本剧","t_pid":"3","t_title":""},
 
     {"t_id":10000,"t_name":"国产动漫","t_pid":"4","t_title":""},
     {"t_id":11000,"t_name":"日韩动漫","t_pid":"4","t_title":""},
